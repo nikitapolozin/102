4d852eddd954975b4430271a028e4e8c2f1c24d2
nikitapolozin <62433160+nikitapolozin@users.noreply.github.com>
2022-11-09 00:46:14 +0300

    Update 17.php

--- 17.php
+++ 17.php
@@ -35,3 +35,31 @@
 //     echo "<option value='{$year}'>{$year}</option>";
 // }
 // echo '</select>';
+
+
+// используя цикл while, реализовать таблицу умножения
+
+
+// echo '<table border="3px solid" width="1000px">';
+// $st = 1;
+// while ($st <= 9) {
+//     echo "<tr>";
+//         $td = 1;
+//         while ($td <= 9) {
+//             echo "<td style='text-align: center; border: 2px solid'>$td * $st = ". $st * $td ." </td>";
+//             $td++;
+//         }
+//     $st++;
+// } echo "</tr>";
+// $tr++;
+// echo "</table>";
+
+echo '<table border="3px solid" width="1000px">';
+for ($st = 1; $st <= 9; $st++) {
+    echo "<tr>";
+    for ($td= 1; $td <= 9; $td++) {
+        echo "<td style='text-align: center; border: 2px solid'>$td * $st = ". $st * $td ." </td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
