ceaa63ee390335f46c3881c7b266a8a04b49c2e7
nikitapolozin <62433160+nikitapolozin@users.noreply.github.com>
2022-11-09 00:39:06 +0300

    Update 17.php

--- 17.php
+++ 17.php
@@ -6,6 +6,32 @@
 //     $i++;
 // }
 
-for ($i = 1; $i <= 10; $i++) {
-    echo "{$i}<br>";
-}
+
+// for ($i = 1; $i <= 10; $i++) {
+//     echo "{$i}<br>";
+// }
+
+
+// for ($i = 20; $i >= 1; $i--) {
+//     echo "{$i}<br>";
+// }
+
+
+// Используя цикл for, реализуйте вывод выпадающего списка select с годами от 1900 до 2021 года включительно
+
+//
+// старый пример
+// $year = 1900;
+// echo '<select>';
+//     while ($year <= 2021) {
+//         echo "<option value='{$year}'>{$year}</option>";
+//         $year++;
+//     }
+// echo '</select';
+
+
+// echo '<select>';
+// for ($year = 2021; $year >= 1900; $year--) {
+//     echo "<option value='{$year}'>{$year}</option>";
+// }
+// echo '</select>';
