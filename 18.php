3f461a99a81459c752236d4b0bde2802dba4521f
nikitapolozin <62433160+nikitapolozin@users.noreply.github.com>
2022-11-11 00:13:10 +0300

    lesson 18

---
+++ 18.php
@@ -0,0 +1,42 @@
+<?php error_reporting(-1)
+
+$i = 4;
+
+if ($i == 1) {
+    echo 1;
+}   elseif ($i == 2){
+    echo 2;
+}   elseif ($i == 3){
+    echo 3;
+}   else {
+    echo 4;
+}
+
+switch ($i) {
+    case '1':
+        echo 1;
+        break;
+    case '2':
+        echo 2;
+        break;
+    case '3':
+        echo 3;
+        break;
+    case '4':
+        echo 4;
+        break;
+    default:
+        echo 5;
+}
+
+switch ($i) {
+    case 1:
+    case 2:
+    case 3:
+        echo "Переменная < 4";
+        break;
+    case 4:
+        echo 4;
+    case 5:
+        echo "Переменная > 5"
+}
